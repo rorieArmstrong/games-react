b5c7a41e26f07d04d85662b771ef41e69defddb9
rorieArmstrong <61753930+rorieArmstrong@users.noreply.github.com>
2022-03-09 20:41:08 +0000

    add onClick function

--- src/pages/NaughtsAndCrosses.js
+++ src/pages/NaughtsAndCrosses.js
@@ -15,10 +15,27 @@ export default class componentName extends Component {
                 8:"",
                 9:""
             },
-
+            turn:"X",
+            win:false
         }
     }
     
+    IsWin = (board) => {
+        if(board[1] === board[2]=== board[3]){return board[1]};
+        if(board[1] === board[5]=== board[9]){return board[1]};
+        if(board[1] === board[4]=== board[7]){return board[1]};
+        if(board[2] === board[5]=== board[8]){return board[2]};
+        if(board[3] === board[5]=== board[7]){return board[3]};
+        if(board[3] === board[6]=== board[9]){return board[3]};
+        if(board[4] === board[5]=== board[6]){return board[4]};
+        if(board[7] === board[8]=== board[9]){return board[7]};
+        return false
+    }
+
+    onClick = (e, turn) => {
+        
+    }
+
     render() {
         return (
             <div> textInComponent </div>
