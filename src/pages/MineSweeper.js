45b74526f38506f7e2fbb1da94e0a4df3ae92ae9
rorieArmstrong <61753930+rorieArmstrong@users.noreply.github.com>
2022-03-12 13:48:59 +0000

    Start of minesweeper

---
+++ src/pages/MineSweeper.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class MineSweeper extends Component {
+    constructor(props) {
+        super(props);
+        
+    }
+    
+    render() {
+        return (
+            <div> textInComponent </div>
+        );
+  }
+}
